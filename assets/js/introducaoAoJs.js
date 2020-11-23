1395d0171bafc5fcd1f127878262ec182d9cda06
Ronaldo P <datascience@gmail.com>
2020-11-23 13:55:21 -0300

    Introdução de js

---
+++ assets/js/introducaoAoJs.js
@@ -0,0 +1,37 @@
+function clicou() {
+    document.getElementById('agradecimento').innerHTML = 'Obrigado por clicar';
+}
+
+function redirecionar () {
+    window.open('https://github.com/Ronaldop819/Introdu-o-ao-js--DIO-');
+    //window.location.href = ('https://github.com/Ronaldop819/Introdu-o-ao-js--DIO-');
+}
+
+function trocar () {
+    document.getElementById('mousemove').innerHTML = 'Obrigado por me ativar';
+}
+
+
+
+
+
+//let lista = ["maça", "pêra", "laranja"];
+//lista.push('uva');
+//lista.pop();
+//console.log(lista);
+
+//let d = new Date();
+//alert(d);
+
+//function validaIdade (idade) {
+  //  let validar;
+ //   if (idade >=18) {
+ //       validar = true;
+ //   }else {
+ //       validar = false;
+  //  }
+   // return validar;
+//}
+
+//let idade = prompt ('Qual a sua idade?');
+//console.log (validaIdade(idade));
